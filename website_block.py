7465d3fc8416b834b81e7481d83c7d94fd7f01a7
pooja srinivasan <58687165+poojaseenu1999@users.noreply.github.com>
2020-07-16 00:18:07 +0530

    adding the ip address of website to block in hosts file

--- website_block.py
+++ website_block.py
@@ -1,8 +1,21 @@
 import time
+from datetime import datetime as dt
+
+hosts_temp="hosts"
 hosts_path=r"C:\Windows\System32\drivers\etc"
 redirect="127.0.0.1"
 website_list=["www.facebook.com","facebook.com"]
 
 while True:
-    print(1)
+    if dt(dt.now().year,dt.now().month,dt.now().day,0) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,3):
+         print("woking hours.....")
+         with open(hosts_temp,'r+') as file:
+             content=file.read()
+             for website in website_list:
+                 if website in content:
+                     pass
+                 else:
+                    file.write(redirect+" "+ website+"\n")
+    else:
+        print("fun hours.....")
     time.sleep(5)
