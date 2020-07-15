7812cc8e7f9552ef9eaa5bd4b5cece87cb07f943
pooja srinivasan <58687165+poojaseenu1999@users.noreply.github.com>
2020-07-16 00:49:25 +0530

    modifying the hosts file

--- website_block.py
+++ website_block.py
@@ -7,7 +7,7 @@ redirect="127.0.0.1"
 website_list=["www.facebook.com","facebook.com"]
 
 while True:
-    if dt(dt.now().year,dt.now().month,dt.now().day,0) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,3):
+    if dt(dt.now().year,dt.now().month,dt.now().day,0) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,4):
          print("woking hours.....")
          with open(hosts_temp,'r+') as file:
              content=file.read()
@@ -17,5 +17,12 @@ while True:
                  else:
                     file.write(redirect+" "+ website+"\n")
     else:
+        with open(hosts_temp,'r+') as file:
+            content=file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
         print("fun hours.....")
     time.sleep(5)
